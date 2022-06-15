11e07607a1198b674ef79d780362a7ac3319e117
Asti <astistateofmind@gmail.com>
2022-06-15 16:04:15 -0700

    define route on the server

--- routes/skills.js
+++ routes/skills.js
@@ -8,6 +8,9 @@ const skillsCtrl = require('../controllers/skills');
  // All actual paths start with "/todos"
 //index route
 router.get('/', skillsCtrl.index);
+//new route
+//position matters in this case
+router.get('/new', skillsCtrl.new)
 //show route
 router.get('/:id', skillsCtrl.show)
 
